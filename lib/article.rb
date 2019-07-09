cfbfbf7997e1f38bac261c99db7884ab8d1d43bc
alexvirga <github email address>
2019-07-09 17:05:38 -0400

    Built out CLI Scaffolding

--- lib/article.rb
+++ lib/article.rb
@@ -6,31 +6,31 @@ class Article < ActiveRecord::Base
     has_many :favorites
     has_many :users, through: :favorites
 
-# def self.news
-#     response_string = RestClient.get("https://newsapi.org/v2/top-headlines?country=us&apiKey=19c889cb66ec40159a52b7cefa4c2b6c")
-#     response_hash = JSON.parse(response_string)
-# end
+def self.news
+    response_string = RestClient.get("https://newsapi.org/v2/top-headlines?country=us&apiKey=19c889cb66ec40159a52b7cefa4c2b6c")
+    response_hash = JSON.parse(response_string)
+end
 
-# def self.news_list
-#     # article = 
-#     # self.news.map do |data,info|
-#     #     if data == "articles"   
-#     #         info
-#     #     end           
-#     # end.compact.flatten
+def self.news_list
+    article =
+    self.news.map do |data,info|
+        if data == "articles"
+            info
+        end
+    end.compact.flatten
 
-#     # news = 
-#     # article.each do |hash|
-#     #     Article.create(
-#     #         title: hash["title"],
-#     #         description: hash["description"],
-#     #         url: hash["url"],
-#     #         content: hash["content"],
-#     #         publishedAt: hash["publishedAt"])
-#     # end
+    news =
+    article.each do |hash|
+        Article.create(
+            title: hash["title"],
+            description: hash["description"],
+            url: hash["url"],
+            content: hash["content"],
+            publishedAt: hash["publishedAt"])
+    end
 
-#     news
+    news
 
-# end
+end
 
 end
