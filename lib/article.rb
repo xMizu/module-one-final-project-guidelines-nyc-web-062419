1eae4c55a388bdeeb45ca30c1dc41017f93db495
Anthony Wan <Anthonywan00@gmail.com>
2019-07-09 12:46:53 -0400

    fixed issues with multiple entries on articles db

--- lib/article.rb
+++ lib/article.rb
@@ -6,31 +6,31 @@ class Article < ActiveRecord::Base
     has_many :favorites
     has_many :users, through: :favorites
 
-# def self.news
-#     response_string = RestClient.get("https://newsapi.org/v2/top-headlines?country=us&apiKey=19c889cb66ec40159a52b7cefa4c2b6c")
-#     response_hash = JSON.parse(response_string)
-# end
+def self.news
+    response_string = RestClient.get("https://newsapi.org/v2/top-headlines?country=us&apiKey=19c889cb66ec40159a52b7cefa4c2b6c")
+    response_hash = JSON.parse(response_string)
+end
 
-# def self.news_list
-#     # article = 
-#     # self.news.map do |data,info|
-#     #     if data == "articles"   
-#     #         info
-#     #     end           
-#     # end.compact.flatten
+def self.news_list
+    article = self.news["articles"]
 
-#     # news = 
-#     # article.each do |hash|
-#     #     Article.create(
-#     #         title: hash["title"],
-#     #         description: hash["description"],
-#     #         url: hash["url"],
-#     #         content: hash["content"],
-#     #         publishedAt: hash["publishedAt"])
-#     # end
+    news = 
+    article.each do |hash|  
+        # find if the article exists based on title
+        if Article.find_by(title: hash["title"])
 
-#     news
-
-# end
+        else
+        # if it exists do nothing
+        # else if it does not exist create new article
+        Article.create(
+            title: hash["title"],
+            description: hash["description"],
+            url: hash["url"],
+            content: hash["content"],
+            publishedAt: hash["publishedAt"])
+        end
+    end
+    news
+end
 
 end
