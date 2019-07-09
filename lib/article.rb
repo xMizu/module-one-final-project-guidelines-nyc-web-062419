383d2ca8dfb6f517e9d2cc26444b034308a20f4e
Anthony Wan <Anthonywan00@gmail.com>
2019-07-09 12:00:17 -0400

    Added code to push data into db

--- lib/article.rb
+++ lib/article.rb
@@ -6,46 +6,31 @@ class Article < ActiveRecord::Base
     has_many :favorites
     has_many :users, through: :favorites
 
-def self.news
-    response_string = RestClient.get("https://newsapi.org/v2/top-headlines?country=us&apiKey=19c889cb66ec40159a52b7cefa4c2b6c")
-    response_hash = JSON.parse(response_string)
-end
-
-def self.news_list
-    self.news.map do |data,info|
-        data == "articles"               
-    end
-end
-
-
-
-
-# def query
-#     puts "hello"
-#     input = ""
-#     input = gets.chomp
-# case input
-# when "headlines"
-#     puts "Pleases select country"
-#     print "United States"
-#     print "United Kingdom"
-#     country = gets.chomp
-#     if country == "United States"
-#         country = "us"
-#     else country == "United Kingdom"
-#         country = "uk"
-#     end
-#     news("top-headlines?country=#{country}")
-# when "search by categories"
-#     list = ["Business", "Entertainment", "Science","Sports","Technology"]
-#     search = gets.chomp
-#     list.include?(search)
-#     news("everything?q=#{search.lower}")
-# when "exit"
-#     exit
-# else
-#     puts "Menu"
+# def self.news
+#     response_string = RestClient.get("https://newsapi.org/v2/top-headlines?country=us&apiKey=19c889cb66ec40159a52b7cefa4c2b6c")
+#     response_hash = JSON.parse(response_string)
 # end
+
+# def self.news_list
+#     # article = 
+#     # self.news.map do |data,info|
+#     #     if data == "articles"   
+#     #         info
+#     #     end           
+#     # end.compact.flatten
+
+#     # news = 
+#     # article.each do |hash|
+#     #     Article.create(
+#     #         title: hash["title"],
+#     #         description: hash["description"],
+#     #         url: hash["url"],
+#     #         content: hash["content"],
+#     #         publishedAt: hash["publishedAt"])
+#     # end
+
+#     news
+
 # end
 
 end
